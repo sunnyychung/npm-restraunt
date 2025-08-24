c950c16e6bd1993dbaf4bbc285d38a0a433d356c
sunnyychung <chungsunny44@gmail.com>
2025-08-24 02:43:23 +0100

    Fixed dynamic scaling of assets and text for phone users

--- src/modules/location.js
+++ src/modules/location.js
@@ -12,9 +12,9 @@ function loadLocation() {
 
         const info = document.createElement("div");
         const googleMaps = document.createElement("iframe");
-        googleMaps.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d608922.8307281829!2d-4.100316268750003!3d53.404008899999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487b21a472e746a3%3A0x628d5476aabc9294!2sGong%20Cha%20Bold%20Street!5e0!3m2!1sen!2suk!4v1755993617713!5m2!1sen!2suk";
-        googleMaps.width = "600";
-        googleMaps.height = "450";
+        googleMaps.src = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1217845.6585939028!2d-2.979711!3d53.404009!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487b21a472e746a3%3A0x628d5476aabc9294!2sGong%20Cha%20Bold%20Street!5e0!3m2!1sen!2suk!4v1755999591068!5m2!1sen!2suk";
+        googleMaps.width = "400";
+        googleMaps.height = "300";
 
         const gcOutside = document.createElement("img");
         gcOutside.src = gcShop;
